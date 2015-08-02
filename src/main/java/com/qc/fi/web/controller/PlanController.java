9ce1513b40a96003598e152ff5b30d9fdaa601df
Lindsey <943238432@qq.com>
2015-08-02 21:00:46 +0800

    add json example

--- src/main/java/com/qc/fi/web/controller/PlanController.java
+++ src/main/java/com/qc/fi/web/controller/PlanController.java
@@ -4,7 +4,9 @@ import com.qc.fi.model.Plan;
 import com.qc.fi.service.PlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -19,5 +21,11 @@ public class PlanController {
     public ModelAndView list() {
         List<Plan> plans = planService.findAll();
         return new ModelAndView("/plan/list", "plans", plans);
+    }
+
+    @RequestMapping("/{id}")
+    public @ResponseBody Plan get(@PathVariable("id") long id) {
+        Plan plan = planService.findOne(id);
+        return plan;
     }
 }
